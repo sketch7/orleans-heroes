13bef3874c36975af81a6c1efb8deaba68c7e333
Stephen Lautier <stephen.lautier@outlook.com>
2018-02-21 00:19:16 +0100

    feat(silo): logging, config, appinfo, hostingenv

---
+++ Heroes.SiloHost.ConsoleApp/Infrastructure/HostingEnvironment.cs
@@ -0,0 +1,25 @@
+﻿namespace Heroes.SiloHost.ConsoleApp.Infrastructure
+{
+	public class HostingEnvironment
+	{
+		public HostingEnvironment()
+		{
+			Environment = System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? ProductionEnvironment;
+			IsDocker = System.Environment.GetEnvironmentVariable("DOCKER") == "true";
+		}
+
+		public static string DevelopmentEnvironment { get; } = "Development";
+		public static string ProductionEnvironment { get; } = "Production";
+
+		public string Environment { get; }
+
+		/// <summary>
+		/// Determine whether its running as docker in development mode. Refrain from use as much as possible and instead use environments.
+		/// </summary>
+		public bool IsDocker { get; }
+
+		public bool IsDockerDev => IsDocker && Environment == DevelopmentEnvironment;
+		public bool IsDev => Environment == DevelopmentEnvironment;
+		public bool IsProd => Environment == ProductionEnvironment;
+	}
+}
--- Heroes.SiloHost.ConsoleApp/Program.cs
+++ Heroes.SiloHost.ConsoleApp/Program.cs
@@ -1,55 +1,133 @@
-﻿using Heroes.Grains;
-using Microsoft.Extensions.Logging;
+﻿using Heroes.Core;
+using Heroes.Grains;
+using Heroes.SiloHost.ConsoleApp.Infrastructure;
+using Microsoft.Extensions.Configuration;
 using Orleans;
 using Orleans.Hosting;
 using Orleans.Runtime.Configuration;
-using System;
-using System.Threading.Tasks;
 using Serilog;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Loader;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Heroes.SiloHost.ConsoleApp
 {
 	public class Program
 	{
-		public static int Main(string[] args)
+		private static ILogger _log;
+		private static ISiloHost _siloHost;
+		private static HostingEnvironment _hostingEnvironment;
+		private static readonly ManualResetEvent SiloStopped = new ManualResetEvent(false);
+		private static Stopwatch _startupStopwatch;
+
+		public static void Main(string[] args)
 		{
-			return RunMainAsync().Result;
+			_log = LoggingConfig.ConfigureSimple()
+				.CreateLogger()
+				.ForContext<Program>()
+				;
+			RunMainAsync(args).Ignore();
+			SiloStopped.WaitOne();
 		}
 
-		private static async Task<int> RunMainAsync()
+		private static async Task RunMainAsync(string[] args)
 		{
 			try
 			{
-				var host = await StartSilo();
-				Console.WriteLine("Press Enter to terminate...");
-				Console.ReadLine();
+				_startupStopwatch = Stopwatch.StartNew();
+				_hostingEnvironment = new HostingEnvironment();
+				var shortEnvName = AppInfo.MapEnvironmentName(_hostingEnvironment.Environment);
+				var configBuilder = new ConfigurationBuilder()
+					.SetBasePath(Directory.GetCurrentDirectory())
+					.AddCommandLine(args)
+					.AddJsonFile("config.json")
+					.AddJsonFile($"config.{shortEnvName}.json")
+					.AddJsonFile("app-info.json")
+					.AddEnvironmentVariables();
 
-				await host.StopAsync();
+				if (_hostingEnvironment.IsDockerDev)
+					configBuilder.AddJsonFile("config.dev-docker.json", optional: true);
 
-				return 0;
+				var config = configBuilder.Build();
+
+				var appInfo = new AppInfo(config);
+				Console.Title = $"Silo - {appInfo.Name}";
+
+				var logger = LoggingConfig.Configure(config, appInfo)
+					.CreateLogger();
+
+				Log.Logger = logger;
+				_log = logger.ForContext<Program>();
+				_log.Information("Initializing Silo {appName} ({version}) [{env}]...", appInfo.Name, appInfo.Version, _hostingEnvironment.Environment);
+
+				_siloHost = BuildSilo(logger);
+				AssemblyLoadContext.Default.Unloading += context =>
+				{
+					_log.Information("Assembly unloading...");
+
+					Task.Run(() => StopSilo(appInfo));
+					SiloStopped.WaitOne();
+
+					_log.Information("Assembly unloaded complete.");
+				};
+
+				await StartSilo();
 			}
 			catch (Exception ex)
 			{
-				Console.WriteLine(ex);
-				return 1;
+				_log.Error(ex, "An error has occurred while initializing or starting silo.");
 			}
 		}
 
-		private static async Task<ISiloHost> StartSilo()
+		private static ISiloHost BuildSilo(ILogger logger)
 		{
 			var config = ClusterConfiguration.LocalhostPrimarySilo();
 			config.AddMemoryStorageProvider();
 
 			var builder = new SiloHostBuilder()
 				.UseConfiguration(config)
+				.ConfigureLogging(logging => logging.AddSerilog(logger, dispose: true))
 				.ConfigureApplicationParts(parts => parts
 					.AddApplicationPart(typeof(HeroGrain).Assembly).WithReferences()
 				)
-				.ConfigureLogging(logging => logging.AddSerilog());
+				//.UseServiceProviderFactory(services =>
+				//{
+				//	return services.AddSingleton(...);
+				//})
+				;
 
-			var host = builder.Build();
-			await host.StartAsync();
-			return host;
+			return builder.Build();
+		}
+
+		private static async Task StartSilo()
+		{
+			_log.Information("Silo initialized in {timeTaken:#.##}s. Starting...", _startupStopwatch.Elapsed.TotalSeconds);
+
+			await _siloHost.StartAsync();
+			_startupStopwatch.Stop();
+
+			_log.Information("Successfully started Silo in {timeTaken:#.##}s (total).", _startupStopwatch.Elapsed.TotalSeconds);
+		}
+
+		private static async Task StopSilo(IAppInfo appInfo)
+		{
+			_log.Information("Stopping Silo '{siloName}'...", appInfo.Name);
+
+			try
+			{
+				await _siloHost.StopAsync();
+			}
+			catch (Exception ex)
+			{
+				_log.Error(ex, "Stopping Silo failed '{siloName}'...", appInfo.Name);
+			}
+
+			_log.Information("Silo '{siloName}' shutdown.", appInfo.Name);
+
+			SiloStopped.Set();
 		}
 	}
 }
