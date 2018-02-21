421c8dae8216ccd197c46d4b16ea6ebf1db0e96e
Stephen Lautier <stephen.lautier@outlook.com>
2018-02-21 21:53:31 +0100

    feat(silo): logging, config, appinfo, hostingenv (#2)
    * feat(deps): update orleans to beta3
    
    * feat(silo): logging, config, appinfo, hostingenv
    
    * logs cleanup + cluster config split
    
    * rename clusterconfig.get to configure for consistency

---
+++ Heroes.Core/AppInfo.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace Heroes.Core
+{
+	public interface IAppInfo
+	{
+		/// <summary>
+		/// Get application name. e.g. '@odin/skeleton'.
+		/// </summary>
+		string Name { get; }
+
+		/// <summary>
+		/// Gets the application short name. e.g. 'skeleton'.
+		/// </summary>
+		string ShortName { get; }
+
+		/// <summary>
+		/// Get environment. e.g. 'Development'. (based on ASPNET_ENVIRONMENT, which can be mapped).
+		/// </summary>
+		string Environment { get; }
+
+		/// <summary>
+		/// Get git short commit hash. e.g. 'b603d6'
+		/// </summary>
+		string GitCommit { get; }
+
+		/// <summary>
+		/// Get application version. e.g. '1.1.0-staging'
+		/// </summary>
+		string Version { get; }
+
+		/// <summary>
+		/// Get whether the app is dockerized or not.
+		/// </summary>
+		bool IsDockerized { get; }
+
+		/// <summary>
+		/// Gets which service type is this app responsible of e.g. web, silo, etc...
+		/// </summary>
+		string ServiceType { get; set; }
+	}
+
+	public class AppInfo : IAppInfo
+	{
+		public string Name { get; set; }
+		public string ShortName { get; }
+		public string Environment { get; set; }
+		public string GitCommit { get; set; }
+		public string Version { get; set; }
+		public bool IsDockerized { get; set; }
+		public string ServiceType { get; set; }
+
+		private static readonly Dictionary<string, string> EnvironmentMapping = new Dictionary<string, string>
+		{
+			["Development"] = "dev",
+			["Staging"] = "staging",
+			["Production"] = "prod",
+		};
+
+		public AppInfo()
+		{
+		}
+
+		/// <summary>
+		/// Resolve from <see cref="IConfiguration"/>.
+		/// </summary>
+		/// <param name="config"></param>
+		public AppInfo(IConfiguration config)
+		{
+			Name = config.GetValue("appName", "app");
+			Version = config.GetValue("version", "local");
+			GitCommit = config.GetValue("gitCommit", "-");
+			Environment = config.GetValue<string>("ASPNETCORE_ENVIRONMENT");
+			IsDockerized = config.GetValue<bool>("DOCKER");
+			ServiceType = config.GetValue("serviceType", "dotnet");
+			ShortName = Name.Split('/').Last();
+
+			if (string.IsNullOrEmpty(Environment))
+				throw new InvalidOperationException("Environment is not set. Please specify the environment via 'ASPNETCORE_ENVIRONMENT'");
+
+			Environment = MapEnvironmentName(Environment);
+		}
+
+		public static string MapEnvironmentName(string environment)
+		{
+			if (environment == null) throw new ArgumentNullException(nameof(environment));
+
+			EnvironmentMapping.TryGetValue(environment, out var env);
+			return env;
+		}
+	}
+}
---
+++ Heroes.SiloHost.ConsoleApp/Infrastructure/ClusterConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using Heroes.Core;
+using Microsoft.Extensions.Configuration;
+using Orleans.Runtime.Configuration;
+
+namespace Heroes.SiloHost.ConsoleApp.Infrastructure
+{
+	public static class ClusterConfig
+	{
+		public static ClusterConfiguration Configure(IConfiguration config, IAppInfo appInfo, HostingEnvironment hostingEnv)
+		{
+			var clusterConfig = appInfo.IsDockerized
+				? new ClusterConfiguration()
+				: ClusterConfiguration.LocalhostPrimarySilo();
+
+			clusterConfig.Globals.ClusterId = $"{appInfo.Name}-{appInfo.Version}";
+			clusterConfig.AddMemoryStorageProvider();
+
+			if (hostingEnv.IsDev)
+				clusterConfig.UseDevelopment();
+			if (appInfo.IsDockerized)
+				clusterConfig.UseDockerSwarm();
+
+			return clusterConfig;
+		}
+
+		public static ClusterConfiguration UseDevelopment(this ClusterConfiguration clusterConfig)
+		{
+			//clusterConfig.AddMemoryStorageProvider(Consts.GrainPersistenceStorage);
+			clusterConfig.Globals.Application.SetDefaultCollectionAgeLimit(TimeSpan.FromMinutes(10));
+			return clusterConfig;
+		}
+
+		public static ClusterConfiguration UseDockerSwarm(this ClusterConfiguration clusterConfig)
+		{
+			clusterConfig.Defaults.PropagateActivityId = true;
+			clusterConfig.Defaults.ProxyGatewayEndpoint = new IPEndPoint(IPAddress.Any, 10400);
+			clusterConfig.Defaults.Port = 10300;
+
+			var ips = Dns.GetHostAddressesAsync(Dns.GetHostName()).Result;
+			var defaultIp = ips.FirstOrDefault();
+
+			clusterConfig.Defaults.HostNameOrIPAddress = defaultIp?.ToString();
+			return clusterConfig;
+		}
+
+	}
+}
