cb92d3fc9c52dc0b259ef7f2c0960f1ec45a2740
Stephen Lautier <stephen.lautier@outlook.com>
2018-10-24 22:51:53 +0200

    update orleans 2.1, .netcore 2.1

--- Heroes.Contracts.Grains/GrainFactoryExtensions.cs
+++ Heroes.Contracts.Grains/GrainFactoryExtensions.cs
@@ -1,21 +1,16 @@
 ﻿using Heroes.Contracts.Grains.Heroes;
 using Heroes.Contracts.Grains.Stats;
 using Orleans;
-using Orleans.Runtime;
 
 namespace Heroes.Contracts.Grains
 {
 	public static class GrainFactoryExtensions
 	{
-		public static IHeroGrain GetHeroGrain(this IGrainFactory factory, string tenant, string key)
-		{
-			return factory.GetGrain<IHeroGrain>($"tenant/{tenant}/{key}");
-		}
+		public static IHeroGrain GetHeroGrain(this IGrainFactory factory, string tenant, string key) 
+			=> factory.GetGrain<IHeroGrain>($"tenant/{tenant}/{key}");
 
-		public static IHeroCollectionGrain GetHeroCollectionGrain(this IGrainFactory factory, string tenant)
-		{
-			return factory.GetGrain<IHeroCollectionGrain>($"tenant/{tenant}");
-		}
+		public static IHeroCollectionGrain GetHeroCollectionGrain(this IGrainFactory factory, string tenant) 
+			=> factory.GetGrain<IHeroCollectionGrain>($"tenant/{tenant}");
 
 		public static IHeroAbilitiesGrain GetHeroAbilitiesGrain(this IGrainFactory factory, string key)
 			=> factory.GetGrain<IHeroAbilitiesGrain>(key);
