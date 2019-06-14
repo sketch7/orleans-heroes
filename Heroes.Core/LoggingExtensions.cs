97625c11b20452cd46806e0e97b209ef7e00744d
Stephen Lautier <stephen.lautier@outlook.com>
2019-06-14 19:36:16 +0200

    remove unused classes + move logging ext

---
+++ Heroes.Core/LoggingExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Serilog;
+
+namespace Heroes.Core
+{
+	public static class LoggingExtensions
+	{
+		public static LoggerConfiguration WithAppInfo(this LoggerConfiguration builder, IAppInfo appInfo)
+			=> builder.Enrich.WithProperty("appName", appInfo.ShortName)
+				.Enrich.WithProperty("serviceType", appInfo.ServiceType)
+				.Enrich.WithProperty("environment", appInfo.Environment)
+				.Enrich.WithProperty("appVersion", appInfo.Version)
+				.Enrich.WithProperty("gitCommit", appInfo.GitCommit);
+	}
+}
