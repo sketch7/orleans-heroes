748b7b44767af67a53a455fdb2068af48d6f9671
Stephen Lautier <stephen.lautier@outlook.com>
2020-06-05 16:44:42 +0200

    rehydrate recentlyviewed

--- heroes.client.ngx-app/src/app/app-store.module.ts
+++ heroes.client.ngx-app/src/app/app-store.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { NgxsModule } from "@ngxs/store";
 import { NgxsReduxDevtoolsPluginModule } from "@ngxs/devtools-plugin";
 import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
+import { NgxsStoragePluginModule } from "@ngxs/storage-plugin";
 
 import { environment } from "../environments/environment";
 import { HeroState } from "./shared";
@@ -17,6 +18,9 @@ import { HeroState } from "./shared";
 		}),
 		NgxsLoggerPluginModule.forRoot({}),
 		NgxsReduxDevtoolsPluginModule.forRoot({
+		}),
+		NgxsStoragePluginModule.forRoot({
+			key: ["heroes.recentlyViewed"]
 		}),
 	],
 })
