a435dced737ea56a0bf8af5ef98107993c194598
Stephen Lautier <stephen.lautier@outlook.com>
2020-06-05 17:42:09 +0200

    update herographtype desc

--- Heroes.Server/Gql/Types/HeroGraphType.cs
+++ Heroes.Server/Gql/Types/HeroGraphType.cs
@@ -8,13 +8,13 @@ namespace Heroes.Server.Gql.Types
 		public HeroGraphType()
 		{
 			Name = "Hero";
-			Description = "A hero object";
+			Description = "A Hero character with unique abilities to play with.";
 
 			Field<StringGraphType>("id", resolve: ctx => ctx.Source.Key);
-			Field(x => x.Key).Description("unique key of a hero.");
+			Field(x => x.Key).Description("Hero unique key.");
 			Field(x => x.Name).Description("Hero name.");
 			Field(x => x.Popularity).Description("Hero popularity.");
-			Field<HeroRoleGraphType>("role", "hero role type.");
+			Field<HeroRoleGraphType>("role", "Hero role type e.g. assassin");
 			Field<ListGraphType<StringGraphType>>("abilities", resolve: ctx => ctx.Source.Abilities, description: "Hero abilities.");
 		}
 	}
