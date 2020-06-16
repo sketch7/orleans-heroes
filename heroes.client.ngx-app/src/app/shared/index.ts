1eaf6de317519a151b6d991fb401b022f9ffce5a
Stephen Lautier <stephen.lautier@outlook.com>
2020-06-16 14:17:09 +0200

    heroState refactor

--- heroes.client.ngx-app/src/app/shared/index.ts
+++ heroes.client.ngx-app/src/app/shared/index.ts
@@ -1,3 +1,7 @@
+import { HeroRealtimeState } from "./state/hero-realtime.state";
+import { HeroCategoryState } from "./state/hero-category.state";
+import { HeroState } from "./state/hero.state";
+
 export * from "./real-time/hero.hubclient";
 export * from "./real-time/hero-hub.model";
 
@@ -16,3 +20,5 @@ export * from "./state/hero.state";
 export * from "./app-info.service";
 
 export * from "./shared.module";
+
+export const HERO_STATE = [HeroState, HeroCategoryState, HeroRealtimeState];
