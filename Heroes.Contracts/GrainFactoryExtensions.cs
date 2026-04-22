1b675d337feeee4cec3f5b3d60e0fa15a8693db5
Stephen Lautier <stephen.lautier@outlook.com>
2026-04-22 12:39:47 +0200

    refactor: update GrainFactoryExtensions for multitenancy support and code organization

--- Heroes.Contracts/GrainFactoryExtensions.cs
+++ Heroes.Contracts/GrainFactoryExtensions.cs
@@ -1,28 +1,32 @@
-﻿using Heroes.Contracts.HeroCategories;
+using Heroes.Contracts.HeroCategories;
 using Heroes.Contracts.Heroes;
 using Heroes.Contracts.Stats;
+using Sketch7.Multitenancy.Orleans;
 
 namespace Heroes.Contracts;
 
 public static class GrainFactoryExtensions
 {
-	public static IHeroGrain GetHeroGrain(this IGrainFactory factory, string tenant, string key)
-		=> factory.GetGrain<IHeroGrain>($"tenant/{tenant}/{key}");
+	extension(IGrainFactory factory)
+	{
+		public IHeroGrain GetHeroGrain(string tenant, string key)
+			=> factory.GetGrain<IHeroGrain>(TenantGrainKey.Create(tenant, key));
 
-	public static IHeroCollectionGrain GetHeroCollectionGrain(this IGrainFactory factory, string tenant)
-		=> factory.GetGrain<IHeroCollectionGrain>($"tenant/{tenant}/collection");
+		public IHeroCollectionGrain GetHeroCollectionGrain(string tenant)
+			=> factory.GetGrain<IHeroCollectionGrain>(TenantGrainKey.Create(tenant));
 
-	public static IHeroCategoryGrain GetHeroCategoryGrain(this IGrainFactory factory, string tenant, string key)
-		=> factory.GetGrain<IHeroCategoryGrain>($"tenant/{tenant}/{key}");
+		public IHeroCategoryGrain GetHeroCategoryGrain(string tenant, string key)
+			=> factory.GetGrain<IHeroCategoryGrain>(TenantGrainKey.Create(tenant, key));
 
-	public static IHeroCategoryCollectionGrain GetHeroCategoryCollectionGrain(this IGrainFactory factory, string tenant)
-		=> factory.GetGrain<IHeroCategoryCollectionGrain>($"tenant/{tenant}/collection");
+		public IHeroCategoryCollectionGrain GetHeroCategoryCollectionGrain(string tenant)
+			=> factory.GetGrain<IHeroCategoryCollectionGrain>(TenantGrainKey.Create(tenant));
 
-	// todo: these should be multi tenant
-	public static IHeroAbilitiesGrain GetHeroAbilitiesGrain(this IGrainFactory factory, string key)
-		=> factory.GetGrain<IHeroAbilitiesGrain>(key);
 
-	// todo: these should be multi tenant
-	public static IHeroStatsGrain GetHeroStatsGrain(this IGrainFactory factory, string key)
-		=> factory.GetGrain<IHeroStatsGrain>(key);
+		// todo: these should be multi tenant
+		public IHeroAbilitiesGrain GetHeroAbilitiesGrain(string key)
+			=> factory.GetGrain<IHeroAbilitiesGrain>(key);
+
+		public IHeroStatsGrain GetHeroStatsGrain(string key)
+			=> factory.GetGrain<IHeroStatsGrain>(key);
+	}
 }
